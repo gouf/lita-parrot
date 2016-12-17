a496179de334f53a6a3fc45abc1e22e73def4723
gouf <innocent.zero@gmail.com>
2016-12-17 21:09:28 +0900

    Convert user id to user name

--- lib/lita/handlers/parrot.rb
+++ lib/lita/handlers/parrot.rb
@@ -5,8 +5,18 @@ module Lita
       route(/parrot/, :parrot)
 
       def parrot(response)
-        response.reply response.user&.id
-        response.reply "@#{response.message.source.user&.name} #{response.message.body}"
+        user_name = find_user_name(response.user&.id) rescue ''
+        response.reply "@#{user_name} #{response.message.body}"
+      end
+
+      def find_user_name(user_id)
+        ::Slack.configure do |config|
+          config.token = ENV['SLACK_API_TOKEN']
+        end
+
+        client = ::Slack::Web::Client.new
+        user_info = client.users_info(user: user_id)
+        user_info.dig(:user, :name)
       end
 
       Lita.register_handler(self)
