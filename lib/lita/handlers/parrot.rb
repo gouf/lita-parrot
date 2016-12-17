c50f58f49389e88b6c559aaead107d7d158b5427
gouf <innocent.zero@gmail.com>
2016-12-17 20:00:14 +0900

    Implement parrot hanlder

--- lib/lita/handlers/parrot.rb
+++ lib/lita/handlers/parrot.rb
@@ -1,7 +1,12 @@
 module Lita
   module Handlers
     class Parrot < Handler
-      # insert handler code here
+      route(/echo/, :parrot)
+      route(/parrot/, :parrot)
+
+      def parrot(response)
+        response.reply "@#{response.message.source.user&.name} #{response.message.body}"
+      end
 
       Lita.register_handler(self)
     end
